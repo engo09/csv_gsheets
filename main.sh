10eb5ceaf44e6f2b4ed5e96d1b46aed18d81b3fa
Enrique <67783497+engo09@users.noreply.github.com>
2024-08-26 21:52:23 +0200

    make it variable for directory

--- main.sh
+++ main.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+MAIN_DIRECTORY_WHERE_CODE_WILL_LIVE=""
+
 if [[ $1 == "new" ]]; then
-    python3 MAIN_DIRECTORY_WHERE_CODE_WILL_LIVE/.csv_sheets/main.py $1 $2;
+    python3 "${MAIN_DIRECTORY_WHERE_CODE_WILL_LIVE}/.csv_sheets/main.py" $1 $2;
 
 elif [[ $1 == "sheet" ]]; then
-    python3 MAIN_DIRECTORY_WHERE_CODE_WILL_LIVE/.csv_sheets/main.py $1 $2;
+    python3 "${MAIN_DIRECTORY_WHERE_CODE_WILL_LIVE}/.csv_sheets/main.py" $1 $2;
 
 else
     echo 'I said "new" or "sheet"!!, bye'
